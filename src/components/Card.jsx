57cd7c9446efb739190ca585c5bc35679d6956b3
shreenarayan1234 <shreenarayanshrestha434@gmail.com>
2025-04-16 12:16:50 +0545

    Card Design Finish

---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+// import image1 from '../assets/image1.avif'
+import { LuLeafyGreen } from "react-icons/lu";
+import { GiChickenOven } from "react-icons/gi";
+
+const Card = ({name,price,type,image}) => {
+  return (
+    <div className='w-[300px] h-[400px] bg-white p-3 rounded-lg flex flex-col gap-3 shadow-lg hover:border-2 border-green-300'>
+        <div className='w-[100%] h-[60%] overflow-hidden rounded-lg'>
+        <img src={image} alt="" className='object-cover' />
+        </div>
+
+        <div className='text-2xl font-semibold'>
+        {name}
+        </div>    
+
+
+        <div className='w-full flex justify-between items-center'>
+            <div className='text-lg font-bold text-green-500'>{price}</div>
+            <div className='flex justify-around item-center text-green-500 text-lg text-semibold gap-2'>{type==="veg"?<LuLeafyGreen />:<GiChickenOven />}<span>{type}</span></div>
+        </div>
+        <button className='bg-green-300 text-gray-700 font-bold text-lg rounded-lg hover:bg-green-400 transaction-all cursor-pointer'>Add to Dish</button>
+    </div>
+  )
+}
+
+export default Card
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,14 +7,14 @@ import { LuShoppingBag } from "react-icons/lu";
 
 const Nav = () => {
   return (
-    <div className='w-full h-[100px] flex justify-between text-center px-8 py-3'>
+    <div className='w-full h-[100px] flex justify-between text-center px-5 md:px-8 py-3'>
         <div className="w-[60px] h-[60px] bg-white flex justify-center items-center rounded-md shadow-xl">
         <IoFastFoodSharp className='w-[30px] h-[30px] text-green-500'/>
         </div>
         <div className="bg-white w-[500px] h-[60px] flex items-center px-5 gap-5 rounded-md shadow-xl">
         <IoSearch className='text-green-500 w-[20px] h-[20px]'/>
-            <form action="">
-                <input type="text" placeholder='Search your dishes......' className='w-[100%] outline-none text-[20px] relative'/>
+            <form action="" className='w-[70%] md:w-[40%]'>
+                <input type="text" placeholder='Search your dishes......' className='outline-none text-[16px] md:text-[20xp] relative '/>
             </form>
         </div>
         <div className="w-[60px] h-[60px] bg-white flex justify-center items-center rounded-md shadow-xl">
